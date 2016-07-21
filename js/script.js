44c6da29d46cea5b6c0a58a32c2923abcc943910
Tanisha <tanishahampden@gmail.com>
2016-07-21 23:11:18 +0200

    added redirected script

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+setTimeout(function(){
+	location.href = 'rocket.html';
+}, 2000);
+
 var doCoolStuff = function () {
 	var currentClassName = document.getElementById('cool').className;
 //changing class name
