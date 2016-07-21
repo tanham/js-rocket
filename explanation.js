8c153955c8a50e256dd089b25edd8dfb6d42d588
Tanisha <tanishahampden@gmail.com>
2016-07-21 18:28:43 +0200

    made my rocket

---
+++ explanation.js
@@ -0,0 +1,66 @@
+
+//Variable Explanation
+var favColor = "green";
+var myFavColors = ["green, purple, yellow"];
+var numOfFavColors = 3;
+var hasGotFavColor = true;
+var myObject = {
+	firstName: "Tanisha",
+	lastName: "Hampden",
+	favColors: ["green, purple, yellow"],
+	yearsAlive: 25,
+	isFemale: true,
+};
+
+//Function Explanation
+Function whatIsMyFavColor () {
+
+}
+
+var whatIsMyFavColor = function () {
+	return "green";
+}
+
+var doSomething = function () {
+	console.log("do Something!!!");
+}
+
+var area = function (width, height) {
+	return width * height;
+}
+
+var fullName = function (firstName, secondName) {
+	return firstName + " " +secondName;
+}
+
+var myName = "Tanisha";
+
+//if-else statement Explanation 
+
+if (true) {
+	//do something 
+	alert("female");
+};
+
+var number1 = 10;
+var number2 = 100;
+
+if (number2 > number1) {
+	//do something 
+	alert(true);
+}
+else {
+	alert(false);
+}
+
+
+var name1 = "Tan";
+var name2 = "Ham";
+
+if (name2 == "Ham" && name1 == "Tan") {
+	//do something 
+	alert(true);
+}
+else {
+	alert(false);
+}
--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
+var doCoolStuff = function () {
+	var currentClassName = document.getElementById('cool').className;
+//changing class name
+	if (currentClassName == 'cool') {
+		document.getElementById('cool').className = 'cool red';
+	} else {
+		document.getElementById('cool').className = 'cool';
+	}
+}
+
+
+
+/*
 var sayMyName = function (name) {
 	alert('My name is: ' +name);
 }
@@ -31,4 +44,4 @@ var car = {
 		}
 
 };
-console.log('hello there friends!');
+*/
